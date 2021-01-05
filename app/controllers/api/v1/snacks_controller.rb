a85a8bb180fdaeab2436675f2fe6658886e68e97
Amodttech <aaron@amodttech.com>
2021-01-05 16:13:27 -0500

    remove from DOM

--- app/controllers/api/v1/snacks_controller.rb
+++ app/controllers/api/v1/snacks_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::SnacksController < ApplicationController
-    # def show
-    #     snack = Snack.find(params[:id])
-    #     render json: snack
-    # end
+    def show
+        snack = Snack.find(params[:id])
+        render json: snack
+    end
 
     def index
         @snacks = Snack.all
         render json: @snacks
+    end
+
+    def create
+        snack = Snack.create(snack_params)
+        render json: snack
+    end
+
+
+    private
+
+    def snack_params
+        params.permit(:name, :bio, :recipe, :image_url)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :snacks, only: [:index]
+      resources :snacks, only: [:index, :create, :show]
       resources :likes, only: [:index, :create]
     end
   end
